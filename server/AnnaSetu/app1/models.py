8a5b1cbf71c3241b6b34b18d0f83a7f831932e44
shreejith <shreejithvm205@gmail.com>
2026-06-30 15:06:54 +0530

    model building

--- server/AnnaSetu/app1/models.py
+++ server/AnnaSetu/app1/models.py
@@ -39,18 +39,19 @@ class Category(models.Model):
     ("GRAIN", "Grain"),
     ("DESSERT", "Dessert"),
 )
-    name = models.CharField(max_length=100,choices=CATEGORY_OPTIONS, default="MEAL")    
+    name = models.CharField(max_length=100,choices=CATEGORY_OPTIONS, default="MEAL")  
+    expiry_time = models.DateTimeField()
+  
 
 
 class FoodListing(models.Model):
     seller=models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    food_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(max_length=200, blank=True, null=True)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="foods/")
-    expiry_time = models.DateTimeField()
     status = models.CharField(
         max_length=20,
         choices=[
@@ -62,3 +63,117 @@ class FoodListing(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)    
+
+
+class Order(models.Model):
+    food = models.ForeignKey(FoodListing, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("PENDING", "Pending"),
+            ("ACCEPTED", "Accepted"),
+            ("COMPLETED", "Completed"),
+            ("CANCELLED", "Cancelled"),
+        ]
+    )
+class PaymentModel(models.Model):
+    PAYMENT_STATUS = (
+        ("CREATED", "Created"),
+        ("SUCCESS", "Success"),
+        ("FAILED", "Failed"),
+    )
+
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    razorpay_order_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_signature = models.CharField(max_length=255, blank=True, null=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=10,choices=PAYMENT_STATUS, default="CREATED")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking.id} - {self.status}" 
+
+
+
+
+class Review(models.Model):
+    food = models.ForeignKey(FoodListing, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(
+        User,
+        related_name="seller_reviews",
+        on_delete=models.CASCADE
+    )
+
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Wishlist(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    food = models.ForeignKey(FoodListing, on_delete=models.CASCADE)
+
+
+class Conversation(models.Model):
+    user1 = models.ForeignKey(
+        User,
+        related_name="user1",
+        on_delete=models.CASCADE )
+
+    user2 = models.ForeignKey(
+        User,
+        related_name="user2",
+        on_delete=models.CASCADE )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE
+    )
+
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+
+
+class Report(models.Model):
+    reporter = models.ForeignKey(
+        User,
+        related_name="reporter",
+        on_delete=models.CASCADE)
+
+    reported_user = models.ForeignKey(
+        User,
+        related_name="reported",
+        on_delete=models.CASCADE )
+
+    reason = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("PENDING", "Pending"),
+            ("RESOLVED", "Resolved"),
+        ]
+    )    
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+   
