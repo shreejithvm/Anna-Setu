41867b60828688947a4f554578918f23d8fd15f2
shreejith <shreejithvm205@gmail.com>
2026-07-09 12:22:14 +0530

    UI page almost correct

--- client/AnnaSetu/src/App.jsx
+++ client/AnnaSetu/src/App.jsx
@@ -2,7 +2,7 @@ import {Route,Routes} from 'react-router-dom'
 import IfNotLogin from "./Components/IfNotLogin"
 import Register from './Components/Register'
 import Login from './Components/Login'
-
+import Home from './Components/Home'
 
 function App() {
   
@@ -10,9 +10,10 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<IfNotLogin/>}/>
+      <Route path="ifnot" element={<IfNotLogin/>}/>
       <Route path="reg" element={<Register/>}/>
       <Route path="log" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
 
     </Routes>
 
